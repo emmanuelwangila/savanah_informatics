b20fa4f93902691e9013bd08be8c74796bdae050
emmanuelwangila <emmanuelwangila1@gmail.com>
2025-05-27 16:32:25 +0300

    Product serializers and categries

---
+++ products/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers ;
+from .models import Products , Category 
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'name' , 'parent', 'children'] 
+
+        def get_children(self , obj):
+            return CategorySerializer(obj.get_children(), many=True).data
+        
+class ProductSerializer(serializers.ModelSerializer):
+    categories = CategorySerializer(many=True, read_only = True);
+    category_ids = serializers.PrimaryKeyRelatedField(
+        queryset=Category.objects.all(),
+        source='categories',
+        many=True,
+        write_only=True
+    )
+    class Meta:
+        model = Products
+        fileds = ['id', 'name', 'description', 'price', 'categories', 'created_at', 'updated_at']        
