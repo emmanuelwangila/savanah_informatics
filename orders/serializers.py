9e3e8417c7e55a02dbe3f89fe752e7ac0f570575
emmanuelwangila <emmanuelwangila1@gmail.com>
2025-05-28 12:41:51 +0300

    Order serailizers and order items serailizers

---
+++ orders/serializers.py
@@ -0,0 +1,52 @@
+from rest_framework import serializers
+from products.serializers import ProductSerializer 
+from.models import Order , OrderItem
+
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    product = ProductSerializer(read_only = True)
+    product_id = serializers.PrimaryKeyRelatedField(
+        queryset = ProductSerializer().Meta.model.objects.all(),
+        source = 'product',
+        write_only = True
+    )
+
+    class Meta:
+        model = OrderItem
+        fields = ['id' , 'product', 'product_id', 'quantity', 'price']
+
+class OrderSerializer(serializers.ModelSerializer):
+    items = OrderItemSerializer(many=True)
+    customer = serializers.StringRelatedField(read_only=True)
+
+
+    class Meta:
+        model = Order
+        fields = [
+            'id', 'customer', 'status', 'total',
+            'items', 'created_at', 'updated_at'
+        ]
+        read_only_fields = ['total', 'created_at', 'updated_at']
+
+
+    def create(self, validated_data):
+        items_data = validated_data.pop('items')
+        order = Order.objects.create(**validated_data)
+        total = 0
+        
+        for item_data in items_data:
+            product = item_data['product']
+            quantity = item_data['quantity']
+            unit_price = product.price
+            
+            OrderItem.objects.create(
+                order=order,
+                product=product,
+                quantity=quantity,
+                unit_price=unit_price
+            )    
+
+
+
+
