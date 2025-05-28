b269802ce0911263e738c77f4daf0ceb9d7c3242
emmanuelwangila <emmanuelwangila1@gmail.com>
2025-05-28 13:39:41 +0300

    Register products to admin panel

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from mptt.admin import MPTTModelAdmin
+from .models import Category, Products
+
 
 # Register your models here.
+class CategoryAdmin(MPTTModelAdmin):
+    list_display = ('name', 'parent')
+    search_fields = ('name',)
+
+# Register Products
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'created_at', 'updated_at')
+    search_fields = ('name', 'description')
+    list_filter = ('categories', 'created_at')
+
+# Register models
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Products, ProductsAdmin)
+
+
