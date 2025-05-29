e1b087f125d463dd596bf0368b653113e2565441
emmanuelwangila <emmanuelwangila1@gmail.com>
2025-05-29 23:06:23 +0300

    Start test functions for order items

--- orders/tests.py
+++ orders/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from rest_framework.test import APITestCase
+from django.urls import reverse
+from .models import Order, OrderItem
+from django.contrib.auth.models import get_user_model 
+
+Customer = get_user_model()
+
+
 
 # Create your tests here.
+class OrderAPITestCase(APITestCase):
