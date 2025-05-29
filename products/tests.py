08db5810f136f170dcfd51e9d7e72d4c14239e2f
emmanuelwangila <emmanuelwangila1@gmail.com>
2025-05-29 20:57:43 +0300

    Refactor test cases

--- products/tests.py
+++ products/tests.py
@@ -15,21 +15,28 @@ class ProductAPITestCase(APITestCase):
             category=self.category
 
         )
-        def test_get_product_list(self):
-            response = self.client.get(reverse('product-list'))
-            self.assertEqual(response.status_code , 200)
-            self.assertEqusl(len(response.data), 1)
-            self.assertEqual(response.data[0]['name'], self.product.name)
-            self.assertEqual(response.data[0]['category'], self.category.id)
+    def test_get_product_list(self):
+        response = self.client.get(reverse('product-list'))
+        self.assertEqual(response.status_code , 200)
+        self.assertEqusl(len(response.data), 1)
+        self.assertEqual(response.data[0]['name'], self.product.name)
+        self.assertEqual(response.data[0]['category'], self.category.id)
 
-        def test_category_average_price(self):
-            response = self.client.get(
-                reverse('category-average-price', args=[self.category.id])
-            )
-            self.assertEqual(response.status_code, 200)
-            self.assertEqusl(response.data['average-price'], self.product.price)
-            self.assertEqual(response.data['category'], self.category.id)
-            self.assertEqual(response.data['category_name'], self.category.name)
+    def test_category_average_price(self):
+        response = self.client.get(
+            reverse('category-average-price', args=[self.category.id])
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['average-price'], self.product.price)
+        self.assertEqual(response.data['category'], self.category.id)
+        self.assertEqual(response.data['category_name'], self.category.name)
+    
+    
+    
+    
+    
+    
+    
 
 
              
