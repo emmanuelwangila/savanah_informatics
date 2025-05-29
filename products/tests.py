17a5f420d7e8ab40250b108bb2a0188c584e7269
emmanuelwangila <emmanuelwangila1@gmail.com>
2025-05-29 20:19:58 +0300

    product and categories test cases

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from django.urls import reverse 
+from rest_framework import APITestCase 
+from .models import Products ,Category
 
 # Create your tests here.
+
+class ProductAPITestCase(APITestCase):
+    def setUp(self):
+        self.category = Category.objects.create(name="Electronics")
+        self.product =Products.objects.create(
+            name="Smartphone",
+            description ="Latest smart phone",
+            price = 699.99,
+            category=self.category
+
+        )
+        
