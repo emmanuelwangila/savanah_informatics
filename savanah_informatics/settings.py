325bacc06fad72de6ab6ec6d06b5a08b8eac4447
emmanuelwangila <emmanuelwangila1@gmail.com>
2025-05-30 20:07:43 +0300

    Set up postgress DB

--- savanah_informatics/settings.py
+++ savanah_informatics/settings.py
@@ -112,12 +112,21 @@ WSGI_APPLICATION = 'savanah_informatics.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'savanah'),
+        'USER': os.getenv('POSTGRES_USER', 'savanahuser'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'savanahpass'),
+        'HOST': os.getenv('POSTGRES_HOST', 'db'),
+        'PORT': os.getenv('POSTGRES_PORT', '5432'),
     }
 }
+
+
 
 
 # Password validation
