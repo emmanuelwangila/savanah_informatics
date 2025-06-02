37cc7851dd96c01504847186f47b227e661163e5
emmanuelwangila <emmanuelwangila1@gmail.com>
2025-06-02 16:54:19 +0300

    Update admin page for orders fetching error and permisions

--- orders/admin.py
+++ orders/admin.py
@@ -9,7 +9,7 @@ class OrderItemInline(admin.TabularInline):
     extra = 1  # Number of empty forms to display
 
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ('id', 'customer', 'status', 'total_amount', 'created_at', 'updated_at')
+    list_display = ('id', 'customer', 'status',  'total_amount', 'created_at', 'updated_at')
     list_filter = ('status', 'created_at')
     search_fields = ('customer__username', 'id')
     inlines = [OrderItemInline]  # Show OrderItems inline in Order
